65daead055ddaa27be184305c1ac2bda8e4b2923
saied63 <saied.rajabgmail.com>
2025-08-13 15:21:39 +0330

    befor put weatherr into api part
    (static reading justn work for wasm not maui)

---
+++ OnionGolmehr.UI.Shared/Layouts/LogoBar.razor.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace OnionGolmehr.UI.Shared.Layouts;
+
+public partial class LogoBar : ComponentBase
+{
+    public string BrandName { get; set; } = "Golmehr";
+}
